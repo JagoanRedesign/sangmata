0562e0f0fb46aa0fe925c37f0badd10245b30d0a
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-01-20 03:38:43 +0700

    Update sangmata.py

--- sangmata.py
+++ sangmata.py
@@ -188,18 +188,11 @@ dispatcher.add_handler(MessageHandler(Filters.chat_type.private,start))
 dispatcher.add_error_handler(error)
 
 
-updater.start_webhook(listen=PORT,
-                          port=int(PORT),
-                          url_path=TOKEN)
-updater.bot.setWebhook('https://'+HEROKU_APP_NAME+'.herokuapp.com/' + TOKEN)
-updater.start_webhook(listen=PORT,
 
+
+
+updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
-
                       url_path=TOKEN,
-
-                      webhook_url='https://'+HEROKU_APP_NAME+'.herokuapp.com/' + TOKEN)
-
-updater.idle()
-
+                      webhook_url="https://"+HEROKU_APP_NAME+".herokuapp.com/" + TOKEN)
 updater.idle()
